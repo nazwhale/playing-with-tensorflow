aeda5fba38877e0b78a4e4bfbf7c268a2696aff7
Naz <nazmalik08@gmail.com>
2017-05-09 16:17:29 +0100

    Train and evaluate model using cross-entrpy and batch gradient descent

--- MNIST-softmax-regression.py
+++ MNIST-softmax-regression.py
@@ -11,4 +11,25 @@ b = tf.Variable(tf.zeros([10]))
 
 #implement model
 y = tf.nn.softmax(tf.matmul(x, W) + b)
-print(y)
+
+#implement cross-entropy
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(
+  tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+#minimise the loss using gradient descent algo with a learning rate of 0.5
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+#launch model in interactive session
+sess = tf.InteractiveSession()
+
+tf.global_variables_initializer().run()
+
+for _ in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+#does our prediction match the truth?
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
